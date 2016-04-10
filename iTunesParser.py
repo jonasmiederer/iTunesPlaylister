ec680fe29ab7d3e53ab862bcccd9679bf9f4b861
Jonas Miederer <jonasmied@gmail.com>
2016-04-10 23:12:03 +0200

    fixed path

--- iTunesParser.py
+++ iTunesParser.py
@@ -47,7 +47,7 @@ class Playlist:
                 track.name = track_obj['Name'] if 'Name' in track_obj else ''
                 track.artist = track_obj['Artist'] if 'Artist' in track_obj else ''
                 track.duration = int(track_obj['Total Time'] / 1000) if 'Total Time' in track_obj else -1
-                track.path = quote(re.sub(".+?(?=multimedia)", "/share/", track_obj['Location']))
+                track.path = quote(re.sub(".+?(?=ultimedia)", "/share/M", track_obj['Location']))
                 songs.append(track)
         else:
             logger.warn('Playlist {} does not contain any songs.'.format(self.pl['Name']))
